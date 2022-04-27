e1112071f4971084e582e8917b07ca184ea35746
Tijana Dmitrovic-Ilijev <tijanadmi@yahoo.com>
2022-04-27 11:46:53 +0200

    Complite curs 1

--- main.go
+++ main.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 
-	"github.com/tijanadmi/webservice/models"
+	"github.com/tijanadmi/webservice/controllers"
 )
 
 func main() {
-	u := models.User{
-		ID:        2,
-		FirstName: "Tricia",
-		LastName:  "McMillan",
-	}
-
-	fmt.Println(u)
+	controllers.RegisterControllers()
+	http.ListenAndServe(":3000", nil)
 }
